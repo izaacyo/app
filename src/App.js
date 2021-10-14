c5a43eccdb3d159b55f381937361643588321307
izaacyo <mariusppetrut@gmail.com>
2021-10-14 23:30:14 +0300

    created product Page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Home from "./pages/Home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+import Product from "./pages/product/Product";
+import ProductList from "./pages/productList/ProductList";
 
 function App() {
   return (
@@ -22,9 +25,18 @@ function App() {
           <Route path="/user/:userId">
             <User />
           </Route>
+          <Route path="/newUser">
+            <NewUser />
+          </Route>
+          <Route path="/product/:productId">
+            <ProductList />
+          </Route>
+          <Route path="/newproduct">
+            <Product />
+          </Route>
         </Switch>
       </div>
-    </Router>
+    </Router >
   );
 }
 
