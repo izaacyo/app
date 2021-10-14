5cf5ae23e8687534738744845a261be6e77da2b0
izaacyo <mariusppetrut@gmail.com>
2021-10-14 19:36:44 +0300

    sidebar created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import "./App.css"
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 
 function App() {
   return (
     <div className="App">
-
       <Topbar />
+      <div className="container">
+        <Sidebar />
+      </div>
     </div>
   );
 }
