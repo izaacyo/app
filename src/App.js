9f96252df8d4acd5fcbde5cd999caf5281352493
izaacyo <mariusppetrut@gmail.com>
2021-10-14 20:01:08 +0300

    created chart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css"
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <Topbar />
       <div className="container">
         <Sidebar />
+      </div>
+      <div>
+        <Home />
       </div>
     </div>
   );
